fb56fb5659707f0b2a114209e1c1bf53d2965858
bean <205502937+midlifeclutch@users.noreply.github.com>
2025-07-01 10:42:08 +0100

    tabular output; include players for spec_player commands

--- go.mod
+++ go.mod
@@ -5,13 +5,18 @@ go 1.23.5
 require (
 	github.com/golang/geo v0.0.0-20250516193853-92f93c4cb289 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
+	github.com/jedib0t/go-pretty/v6 v6.6.7 // indirect
 	github.com/markus-wa/demoinfocs-golang/v4 v4.3.3 // indirect
 	github.com/markus-wa/go-unassert v0.1.3 // indirect
 	github.com/markus-wa/gobitread v0.2.4 // indirect
 	github.com/markus-wa/godispatch v1.4.1 // indirect
 	github.com/markus-wa/ice-cipher-go v0.0.0-20230901094113-348096939ba7 // indirect
 	github.com/markus-wa/quickhull-go/v2 v2.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/oklog/ulid/v2 v2.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/markus-wa/demoinfocs-golang/v4/pkg/demoinfocs"
 	"github.com/markus-wa/demoinfocs-golang/v4/pkg/demoinfocs/events"
 )
@@ -24,27 +25,58 @@ func main() {
 	p := demoinfocs.NewParser(f)
 	defer p.Close()
 
+	t := table.NewWriter()
+	t.SetStyle(table.StyleLight)
+	t.Style().Options.SeparateRows = true
+
 	p.RegisterEventHandler(func(e events.MatchStart) {
 		gState := p.GameState()
 
 		if clanName := gState.TeamCounterTerrorists().ClanName(); clanName != "" {
+
 			ctEnts := []int{}
+			ctSpecPlayer := make(map[string]int)
 			for _, player := range gState.TeamCounterTerrorists().Members() {
 				ctEnts = append(ctEnts, player.EntityID)
+				ctSpecPlayer[player.Name] = player.EntityID
 			}
 
 			ctEntsDec := binToDec(ctEnts)
-			fmt.Printf("tv_listen_voice_indices %d; tv_listen_voice_indices_h %d // %s\n", ctEntsDec, ctEntsDec, clanName)
+
+			t.SetTitle(fmt.Sprintf(clanName))
+			t.AppendHeader(table.Row{"spec", "name"})
+			t.SetCaption(fmt.Sprintf("tv_listen_voice_indices %d; tv_listen_voice_indices_h %d", ctEntsDec, ctEntsDec))
+
+			for name, idx := range ctSpecPlayer {
+				t.AppendRow(table.Row{idx, name})
+			}
+
+			fmt.Println(t.Render())
+
 		}
+
+		t.ResetRows()
+		fmt.Println()
 
 		if clanName := gState.TeamTerrorists().ClanName(); clanName != "" {
 			tEnts := []int{}
+			tSpecPlayer := make(map[string]int)
 			for _, player := range gState.TeamTerrorists().Members() {
 				tEnts = append(tEnts, player.EntityID)
+				tSpecPlayer[player.Name] = player.EntityID
 			}
 
 			tEntsDec := binToDec(tEnts)
-			fmt.Printf("tv_listen_voice_indices %d; tv_listen_voice_indices_h %d // %s\n", tEntsDec, tEntsDec, clanName)
+
+			t.SetTitle(fmt.Sprintf(clanName))
+			t.SetCaption(fmt.Sprintf("tv_listen_voice_indices %d; tv_listen_voice_indices_h %d", tEntsDec, tEntsDec))
+
+			for name, idx := range tSpecPlayer {
+				t.AppendRow(table.Row{idx, name})
+			}
+
+			fmt.Println(t.Render())
+
 		}
 
 	})
